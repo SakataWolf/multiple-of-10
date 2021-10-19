6bf72404378c052711d426ab92a85b0537054de1
SakataWolf <87884401+SakataWolf@users.noreply.github.com>
2021-10-19 17:38:09 -0300

    Add files via upload

---
+++ Múltiplo de 10/Múltiplo de 10/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Múltiplo_de_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Contador de 1 a 100");
+            Console.WriteLine("Aperte uma tecla para começar");
+            Console.ReadKey();
+            int x;
+            for (x = 1; x <= 100; x++)
+            {
+                Console.Write("\n\n"+x);
+                if (x % 10 == 0)
+                {
+                    Console.Write("  MÚLTIPLO DE 10");
+                } 
+            }
+        }
+    }
+}
